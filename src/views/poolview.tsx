6b0e420f915d21ea32ac8498578c7eeaeb430225
exrobbie <astrorobbie@163.com>
2021-08-13 10:19:16 +0800

    optimized style of pool

--- src/views/poolview.tsx
+++ src/views/poolview.tsx
@@ -1,18 +1,85 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as react from "react";
 
-import { Stack } from "@fluentui/react/lib/Stack";
+// import { Stack } from "@fluentui/react/lib/Stack";
 import { PoolInfo } from "../libs/type";
-import './poolview.scss'
+import EthIcon from "../img/tokens/eth-pure.svg";
+import "./poolview.scss";
 
 interface IProps {
   poolInfoList: PoolInfo[];
 }
 
 export default function PoolView(props: IProps) {
+  const { poolInfoList } = props;
+  console.log(poolInfoList);
   return (
     <>
-      <Stack>
+      <div className="pools-page">
+        <div className="box">
+          <div className="board">
+            <div className="pool-list">
+              {poolInfoList.map((item) => (
+                <div className="pool-item">
+                  <div className="left">
+                    <img src={EthIcon} className="icon" />
+                    <div>
+                      <div className="token-pair">
+                        {item.tokenName1} - {item.tokenName2}
+                      </div>
+                      <div className="chain-pair">
+                        {item.chainName1}[{item.chainId1}] - {item.chainName2}[
+                        {item.chainId2}]
+                      </div>
+                    </div>
+                  </div>
+                  <div className="right">
+                    <div>
+                      Vol: <span className="value">{item.amount}</span>
+                    </div>
+                    <div>
+                      Share: <span className="value">{item.share}</span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            {/* <table className="table">
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Token Name</th>
+                  <th scope="col">Chain Name</th>
+                  <th scope="col">Token Name</th>
+                  <th scope="col">Chain Name</th>
+                  <th scope="col">Vol</th>
+                  <th scope="col">Share</th>
+                </tr>
+              </thead>
+              <tbody>
+                {poolInfoList.map((pi) => (
+                  <tr key="pool-{pi.id}">
+                    <th>{pi.id}</th>
+                    <th>{pi.tokenName1}</th>
+                    <th>
+                      {pi.chainName1}[{pi.chainId1}]
+                    </th>
+                    <th>{pi.tokenName2}</th>
+                    <th>
+                      {pi.chainName2}[{pi.chainId2}]
+                    </th>
+                    <th>{pi.amount}</th>
+                    <th>{pi.share}</th>
+                  </tr>
+                ))}
+              </tbody>
+            </table> */}
+          </div>
+        </div>
+      </div>
+
+      {/* <Stack>
         <table className="table">
           <thead className="thead-dark">
             <tr>
@@ -26,20 +93,24 @@ export default function PoolView(props: IProps) {
             </tr>
           </thead>
           <tbody>
-          {props.poolInfoList.map((pi) => (
-            <tr key="pool-{pi.id}">
-              <th>{pi.id}</th>
-              <th>{pi.tokenName1}</th>
-              <th>{pi.chainName1}[{pi.chainId1}]</th>
-              <th>{pi.tokenName2}</th>
-              <th>{pi.chainName2}[{pi.chainId2}]</th>
-              <th>{pi.amount}</th>
-              <th>{pi.share}</th>
-            </tr>
-          ))}
+            {poolInfoList.map((pi) => (
+              <tr key="pool-{pi.id}">
+                <th>{pi.id}</th>
+                <th>{pi.tokenName1}</th>
+                <th>
+                  {pi.chainName1}[{pi.chainId1}]
+                </th>
+                <th>{pi.tokenName2}</th>
+                <th>
+                  {pi.chainName2}[{pi.chainId2}]
+                </th>
+                <th>{pi.amount}</th>
+                <th>{pi.share}</th>
+              </tr>
+            ))}
           </tbody>
         </table>
-      </Stack>
+      </Stack> */}
     </>
   );
 }
